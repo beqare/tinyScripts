7a846db30b5bba6b16c597d9bd7075da2ba3c48c
Jonas <turtelmaster11@gmail.com>
2024-04-21 23:36:25 +0200

    a

---
+++ lua/Roblox/Portal Trigger.lua
@@ -0,0 +1,23 @@
+-- insert in ServerScriptService
+
+local folder = game.Workspace.Portals -- 1
+
+local function Warp(num: number, hit: BasePart)
+	local char = hit.Parent
+	local hum = char and char:FindFirstChildOfClass("Humanoid")
+	if not hum then return end
+
+	local to = folder:FindFirstChild(`to{num}`)
+	if not to then return end
+	char.HumanoidRootPart.CFrame = to.CFrame * CFrame.new(0, 4, 0)
+end
+
+for _, from in folder:GetChildren() do
+	local regex = "from(%d+)"
+	local num = string.match(from.Name, regex)
+	if not num then continue end
+
+	from.Touched:Connect(function(hit: BasePart)
+		Warp(num, hit)
+	end)
+end
