e88571215fab93d8e3e38a2bbb39e59c4e4e0740
snoopti <turtelmaster11@gmail.com>
2024-04-22 05:13:17 +0200

    a

--- setup.py
+++ setup.py
@@ -1,19 +1,44 @@
 import os
+import time
 import subprocess
 
+output = "./src/output"
+input = "./src/input"
+
+libraries = ["turtle", "pytube", "subprocess", "os", "tkinter", "random"]
+
+
 def create_directories():
-    if not os.path.exists("./src/output"):
-        os.makedirs("./src/output")
-    if not os.path.exists("./src/input"):
-        os.makedirs("./src/input")
+    if not os.path.exists(output):
+        os.makedirs(output)
+
+    if not os.path.exists(input):
+        os.makedirs(input)
+
 
 def install_libraries():
     try:
-        subprocess.run(["pip", "install", "turtle", "pytube", "subprocess", "os", "tkinter", "random"], check=True)
+        subprocess.run(["pip", "install", *libraries], check=True)
+        print("libraries installed")
     except subprocess.CalledProcessError as e:
-        print(f"Fail: {e}")
+        print(f"Fail to install library: {e}")
         return
+
+
+print("")
+print("Creating directories...")
+print("")
+
+create_directories()
+
+print("created...")
+
+time.sleep(1)
+
+print("")
+print("Installing libraries...")
+print("")
 
 install_libraries()
 
-create_directories()
+print("installed")
